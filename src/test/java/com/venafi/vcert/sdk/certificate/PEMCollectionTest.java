e9384a45efc2b8ea82452483e3f0fd66944e7a56
Patrick Mackinder <patrick.mackinder@opencredo.com>
2019-04-04 12:38:39 +0100

    Retrieve certificate for tpp

--- src/test/java/com/venafi/vcert/sdk/certificate/PEMCollectionTest.java
+++ src/test/java/com/venafi/vcert/sdk/certificate/PEMCollectionTest.java
@@ -1,23 +1,23 @@
 package com.venafi.vcert.sdk.certificate;
 
+import com.venafi.vcert.sdk.VCertException;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.security.cert.CertificateException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PEMCollectionTest {
 
     @Test
-    void fromResponse() throws IOException, CertificateException {
+    void fromResponse() throws VCertException, IOException {
         ClassLoader classLoader = getClass().getClassLoader();
         String body = new String(Files.readAllBytes(Paths.get(classLoader.getResource("certificates/certWithKey.pem").getPath())));
-        PEMCollection pemCollection = PEMCollection.fromResponse(body, ChainOption.ChainOptionRootLast);
+        PEMCollection pemCollection = PEMCollection.fromResponse(body, ChainOption.ChainOptionIgnore);
         assertThat(pemCollection.certificate()).isNotNull();
-        assertThat(pemCollection.chain()).hasSize(1);
+        assertThat(pemCollection.chain()).hasSize(0);
         assertThat(pemCollection.privateKey()).isNotNull();
     }
 }
